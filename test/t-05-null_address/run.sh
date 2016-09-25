469cbd5d761561522d4ddb4fc333a2f4cd9f17d8
Alberto Bertogli <albertito@blitiri.com.ar>
2016-09-25 20:05:21 +0100

    test: Add a test for DSN and null address deliveries
    This patch adds a test for delivery status notifications and null address
    deliveries, that check that chasquid can both receive and send DSNs.
    
    To do this, we extend the mail_diff utility to support wildcards in the
    comparisons, to skip over variable parts of the messages (like dates).

---
+++ test/t-05-null_address/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+. $(dirname ${0})/../util/lib.sh
+
+init
+
+generate_certs_for testserver
+add_user testserver user secretpassword
+
+mkdir -p .logs
+chasquid -v=2 --log_dir=.logs --config_dir=config &
+wait_until_ready 1025
+
+
+# Send mail with an empty address (directly, unauthenticated).
+nc localhost 1025 < sendmail > /dev/null
+wait_for_file .mail/user@testserver
+mail_diff content .mail/user@testserver
+rm -f .mail/user@testserver
+
+
+# Test that we get mail back for a failed delivery
+run_msmtp fail@testserver < content
+wait_for_file .mail/user@testserver
+mail_diff expected_dsr .mail/user@testserver
+
+
+success
