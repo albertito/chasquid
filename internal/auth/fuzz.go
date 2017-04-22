9f603da8de42bed7ce7151c9d15e6a2bd6ae34c1
Alberto Bertogli <albertito@blitiri.com.ar>
2017-04-22 14:52:18 +0100

    auth: Fuzz testing

---
+++ internal/auth/fuzz.go
@@ -0,0 +1,16 @@
+// Fuzz testing for package aliases.
+
+// +build gofuzz
+
+package auth
+
+func Fuzz(data []byte) int {
+	//	user, domain, passwd, err := DecodeResponse(string(data))
+	interesting := 0
+	_, _, _, err := DecodeResponse(string(data))
+	if err == nil {
+		interesting = 1
+	}
+
+	return interesting
+}
