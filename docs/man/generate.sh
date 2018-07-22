a2fa1d07d1df887dcb988b4e5064bf61faff786e
Alberto Bertogli <albertito@blitiri.com.ar>
2018-07-22 11:11:08 +0100

    docs/man: Make generate script use git timestamps
    When regenerating the manpages, the mtime of the pod files is used.
    That mtime can change based on file and repository manipulations,
    because git does not preserve mtimes.
    
    This causes unnecessary regenerations which require manual cleanups in
    order to avoid cluttering the history unnecessarily.
    
    This patch makes the generate script set the mtime of the pod files to
    the time of the last git commit that affected them, if they have not
    changed since. This avoids unnecessary changes and makes the script
    easier to use.
    
    There's one file that needed adjustment to match its last commit time,
    that is also included here.

--- docs/man/generate.sh
+++ docs/man/generate.sh
@@ -12,6 +12,14 @@ for IN in *.pod; do
 	SECTION=${OUT##*.}
 	NAME=${OUT%.*}
 
+	# If it has not changed in git, set the mtime to the last commit that
+	# touched the file.
+	CHANGED=$( git status --porcelain -- "$IN" | wc -l )
+	if [ $CHANGED -eq 0 ]; then
+		GIT_MTIME=$( git log --pretty=%at -n1 -- "$IN" )
+		touch -d "@$GIT_MTIME" "$IN"
+	fi
+
 	podchecker $IN
 	pod2man --section=$SECTION --name=$NAME \
 		--release "" --center "" \
