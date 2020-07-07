380da0c1a1db1ed2a3c311f2170e3b762dfb624e
Alberto Bertogli <albertito@blitiri.com.ar>
2020-07-08 00:30:11 +0100

    dovecot-auth-cli: Add help message
    The dovecot-auth-cli binary can be useful to debug and troubleshoot
    dovecot auth issues.
    
    This patch adds a small help message to make it easier for users to
    understand how to use it.

--- cmd/dovecot-auth-cli/dovecot-auth-cli.go
+++ cmd/dovecot-auth-cli/dovecot-auth-cli.go
@@ -8,12 +8,31 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"blitiri.com.ar/go/chasquid/internal/dovecot"
 )
 
+const help = `
+Usage:
+	dovecot-auth-cli <path prefix> exists user@domain
+	dovecot-auth-cli <path prefix> auth user@domain password
+
+Example:
+	dovecot-auth-cli /var/run/dovecot/auth-chasquid exists user@domain
+	dovecot-auth-cli /var/run/dovecot/auth-chasquid auth user@domain password
+
+`
+
 func main() {
 	flag.Parse()
+
+	if len(flag.Args()) < 3 {
+		fmt.Fprint(os.Stderr, help)
+		fmt.Print("no: invalid arguments\n")
+		return
+	}
+
 	a := dovecot.NewAuth(flag.Arg(0)+"-userdb", flag.Arg(0)+"-client")
 
 	var ok bool
@@ -25,11 +44,11 @@ func main() {
 	case "auth":
 		ok, err = a.Authenticate(flag.Arg(2), flag.Arg(3))
 	default:
-		fmt.Printf("unknown subcommand\n")
+		err = fmt.Errorf("unknown subcommand %q", flag.Arg(1))
 	}
 
 	if ok {
-		fmt.Printf("yes\n")
+		fmt.Print("yes\n")
 		return
 	}
 
