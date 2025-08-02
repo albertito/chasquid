380b7d8ff80a41426c2fa0fe9a8cd1c310d06fc9
Guiorgy <Guiorgy123@gmail.com>
2025-08-02 23:56:49 +0400

    use gosu instead of sudo

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -107,4 +107,4 @@ start-stop-daemon --start --quiet --pidfile /run/dovecot.pid \
 	--exec /usr/sbin/dovecot -- -c /etc/dovecot/dovecot.conf
 
 # shellcheck disable=SC2086
-exec sudo -u chasquid -g chasquid /usr/bin/chasquid $CHASQUID_FLAGS
+exec gosu chasquid:chasquid /usr/bin/chasquid $CHASQUID_FLAGS
